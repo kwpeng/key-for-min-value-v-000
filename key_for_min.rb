f681a6095d1e9e9879dc30cf3901d7cfd02ec3d8
bluebeadlily <bluebeadlily@gmail.com>
2018-10-08 00:58:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,13 @@ def key_for_min_value(name_hash)
  
  name_hash.each do |key, value|
    
-    if min == nil || min > value
+    if min == nil
       min = value
       min_key = key
-  else
+  elsif min > value
+      min = value
+      min_key = key
+  
 end
 end
 end
